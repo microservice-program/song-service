b0f637aa06b1f169f46769574de45b3e436b4ad7
Samandar Akbarov <akbarov.samandar@gmail.com>
2023-03-08 16:24:44 +0500

    Fixing issue that has in communication between services

--- src/main/java/app/music/songservice/feign/ResourceClient.java
+++ src/main/java/app/music/songservice/feign/ResourceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(value = "song-resource", url = "http://localhost:8080/api/v1/resources")
+@FeignClient(value = "song-resource", url = "http://localhost:8000/api/v1/resources")
 public interface ResourceClient {
 
     @GetMapping("/{id}/check")
